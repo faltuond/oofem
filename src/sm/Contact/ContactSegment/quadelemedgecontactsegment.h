5a635d03faf2143ed924e95530a27044b34e236b
faltuond <0xdrafon@gmail.com>
2019-12-02 15:31:41 +0100

    Inital commit of QuadraticElementEdgeContactSegment

---
+++ src/sm/Contact/ContactSegment/quadelemedgecontactsegment.h
@@ -0,0 +1,102 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2013   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+
+#pragma once
+#include "contactsegment.h"
+#include "node.h"
+#include "inputrecord.h"
+#include "Elements/structuralelement.h"
+#include "set.h"
+#include "feinterpol.h"
+#include "classfactory.h"
+
+#define _IFT_QuadraticElementEdgeContactSegment_Name "quadraticelementedgecontactsegment"
+#define _IFT_QuadraticElementEdgeContactSegment_edgeSet "edgeset"
+
+namespace oofem {
+    class QuadraticElementEdgeContactSegment : public ContactSegment
+    {
+    public:
+        QuadraticElementEdgeContactSegment(int n, Domain *aDomain) : ContactSegment(n, aDomain) { ; }
+        ~QuadraticElementEdgeContactSegment() {};
+
+        IRResultType initializeFrom(InputRecord * ir);
+
+        //returns normalized n, which is an normal vector of contact
+        void computeNormal(FloatArray& answer, Node * node, TimeStep* tstep) override;
+
+        //returns an extended N (aka A) matrix, integrated at point of contact of given node
+        void computeExtendedNMatrix(FloatMatrix& answer, Node* node, TimeStep * tStep) override;
+
+        //computes the penetration of node given 
+        double computePenetration(Node * node, TimeStep * tStep) override;
+
+        void giveLocationArray(const IntArray& dofIdArray, IntArray& s_loc, const UnknownNumberingScheme& c_s) override;
+
+        void updateYourself(TimeStep * tStep) override;
+
+        const char *giveClassName() const override { return "Elemedgecontactsegment"; }
+        const char *giveInputRecordName() const override { return _IFT_QuadraticElementEdgeContactSegment_Name; }
+
+        void postInitialize() override;
+    private:
+        IntArray edges;
+        IntArray lastEdge; //last edge worked with
+        std::vector<Node*> knownNodes;
+        std::vector<IntArray> knownClosestEdges;
+        int setnum;
+
+        //gives the closest edge to a given node in the form of an IntArray(elempos,edgepos)
+        //only computes it again if it wasn't determined for this node in this solution step yet
+        //(i.e. the array of known closest edges is reset after step convergence)
+        void giveClosestEdge(IntArray& answer, Node * node, TimeStep * tStep);
+
+        //computes distance of a point given by a set of coordinates to a line given by two sets of coordinates
+        //returns whether point of intersection is inbetween the line points
+        //bool computeDistanceVector(FloatArray& answer, const FloatArray& externalPoint, const FloatArray& linePoint1, const FloatArray& linePoint2, FloatArray * contactPointCoords = nullptr);
+
+        //searches the array of nodes to which a closest edge was already computed
+        //returns -1 if unsuccessful
+        inline int giveIndexOfKnownNode(const Node * node) {
+            for ( int i = 0; i < knownNodes.size(); i++ ) {
+                if ( node == knownNodes.at(i) ) return i;
+            }
+            return -1;
+        }
+
+    };
+
+}
